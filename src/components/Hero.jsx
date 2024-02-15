9ccd612bbcff674a007fbe0ea9b8748d08eba91f
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2024-02-16 01:42:59 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,12 @@ const Hero = () => {
         </h1>
         <p className='text-gray-400 md:pt-4 lg:text-lg'>
          This is platform where you can write your throughts -&gt; Article's / Blogs /
-         Post's just like another social media app 
+         Post's just like another social media app  <br />
+         We'll never share your Data with anyone else.  End-to-End encrypted your privacy my policy  &nbsp;  &nbsp; 
+         <a style={{textDecoration:'none'}} className='text-gray-400 md:pt-4 lg:text-lg' href="https://www.termsfeed.com/live/b41df7ba-864d-496e-bd28-c8800be44777">Privacy/Policy1  &nbsp;  &nbsp; </a>
+         <a style={{textDecoration:'none'}} className='text-gray-400 md:pt-4 lg:text-lg' href="https://www.freeprivacypolicy.com/live/e2daa1bc-aaf9-48c9-89a3-0dba9f79d6f0">Privacy/Policy2  &nbsp;  &nbsp; </a>
+         <a style={{textDecoration:'none'}} className='text-gray-400 md:pt-4 lg:text-lg' href="https://www.privacypolicies.com/live/b7c6d0aa-121a-47f3-9e90-f0a09df4c67e">Privacy/Policy3  &nbsp;  &nbsp; </a>
+         <a style={{textDecoration:'none'}} className='text-gray-400 md:pt-4 lg:text-lg' href="https://aryan0-1maurya.github.io/Licence/">Licence</a>
         </p>
       </div>
       <div className='mx-auto max-w-[70%] bg-transparent md:max-w-[50%]'>
